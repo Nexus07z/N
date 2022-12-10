bf988ee20ec5587caa3a9022914992e55cf2ad14
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-10 09:00:00 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -68,7 +68,7 @@ global.mygc = "https://chat.whatsapp.com/Cp1OJenk6Q9D9vgLjLU558"
 // Engak Ngaruh, Belum Kepasang
 global.botname = 'Nexusᴮᴼᵀ'
 global.akulaku = 'Bot By Naze'
-global.ytname = 'YT NAZE' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+
 
 
 let file = require.resolve(__filename)
