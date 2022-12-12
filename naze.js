9928abe323007da0c2b1ef80f4f89c45866bc30a
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-12 11:00:24 -0500

    Add files via upload

--- naze.js
+++ naze.js
@@ -54,6 +54,7 @@ const tengah = moment.tz('America/Lima').format('HH:mm:ss')
 const timur = moment.tz('America/Lima').format('HH:mm:ss')
 const ini_mark = `0@s.whatsapp.net`
 const ownernya = ownernomer + '@s.whatsapp.net'
+const premium = JSON.parse(fs.readFileSync('./database/premium.json'))
 global.prem = require("./lib/premium")
 gambar = fs.readFileSync('./media/image/assistant.jpg')
 
