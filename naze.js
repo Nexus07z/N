b46b53bda594aac98c001de6cf3a3aef7889a086
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-13 10:08:36 -0500

    Add files via upload

--- naze.js
+++ naze.js
@@ -995,7 +995,8 @@ break
             case 'limite': {
                 let me = m.sender
                 let limit = `${isPremium ? `*@${me.split('@')[0]} eres un usuario premium sin límites*` : `*@${me.split('@')[0]} te quedan ${db.data.users[m.sender].limit} de ${global.limituser.free} comandos premium disponibles, se renuevan cada ${global.limituser.resetcron} horas.*`}`
-                naze.sendMessage(m.chat, { text: limit, mentions: participants.map(a => a.id) }, {quoted: m})	
+                naze.sendMessage(m.chat, { text: limit }, {mentions: ments, quoted: m})
+        
 			}
 			break 
 
