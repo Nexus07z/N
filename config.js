297e3b5cd1537d2c60cdb7d2dbd3f50a787bfa54
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-13 11:26:55 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -19,25 +19,17 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['51963324152']
-global.ownernomer = "51963324152"
-global.creador = ['51963324152']
-global.numcreador = "51963324152"
-global.packname = 'Sticker'
-global.author = 'Nexus'
-global.sessionName = 'nexusdev'
+global.owner = ['6285875158363']
+global.ownernomer = "6285875158363"
+global.premium = ['6285875158363']
+global.packname = 'Sticker By'
+global.author = 'Naze Dev'
+global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
 global.jumlah = '1000000000'
-global.prefix = ['.']
+global.prefa = ['','!','.','#','&']
 global.sp = ''
-
-// Limit
-global.limitawal = {
-    premium: "Infinity",
-    free: 25,
-    resetcron:12
-}
 
 // Setting Mess
 global.mess = {
@@ -46,39 +38,36 @@ global.mess = {
     botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
     premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa / Chat Owner',
     owner: 'Fitur Khusus Owner Bot',
-    ownerB: '[❗]\n*¡Este comando solo puede ser utilizado por el creador del Bot!*',
-    group: '[❗]\n*¡Este comando solo puede ser utilizado en grupos!*',
-    grupo: '[❗] ALERTA\n\n*¡Este comando solo puede ser utilizado en grupos!*',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
     private: 'Fitur Digunakan Hanya Untuk Private Chat!',
     bot: 'Fitur Khusus Pengguna Nomor Bot',
-    comandoespera: '*Espera un momento por favor...*',
-    etiquetaimg: 'Debes etiquetar una imagen con el comando: ',
-    etiquetastick: 'Debes etiquetar un sticker con el comando: ',
-    textocomando: 'Falta agregar texto dejando un espacio al lado del siguiente comando: ',
-    linkcomando: 'Falta agregar la url dejando un espacio al lado del siguiente comando: ',
-    etiquetaimgvidl: 'Debes etiquetar una imagen o un video con una duración máxima de 10 segundos con el comando: ',
-    limm: '*La duración máxima permitida es de 10 minutos.*',
-    lims: '*La duración máxima permitida es de 10 segundos.*',
-    error: '*Ocurrió un problema, puedes intentarlo nuevamente más tarde.*',
+    wait: 'Loading...',
+    error: 'Error!',
     errapi: 'Error Mungkin Apikey Tidak Valid!',
     errmor: 'Error Kesalahan Sistem',
-    endLimit: `[❗] *ALERTA*\n\n*No puedes usar este comando premium.*\n\nHas llegado al *límite de comandos premium* que tu *usuario gratuito* puede usar.\n\nTu usuario tiene un límite de uso de ${global.limitawal.free} comandos premium cada ${global.limitawal.resetcron} horas, este límite se restablece cada 12 horas.\n\nPara monitorear cuantos comandos premium tienes disponibles usa el siguiente comando:\n\n*${prefix}limite*\n\nSi te conviertes en *usuario premium* puedes usar los comandos premium *sin limitaciones* en el tiempo que dure tu suscripción.\n\nPara convertirte en usuario premium puedes consultar el siguiente comando:\n\n*${prefix}premium*\n`,
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+
+// Limit
+global.limitawal = {
+    premium: "Infinity",
+    free: 25
 }
 
 // Fake
-global.thumb = fs.readFileSync('./media/image/assistant.jpg')
-global.faall = fs.readFileSync('./media/image/assistant.jpg')
+global.thumb = fs.readFileSync('./media/image/naze.jpg')
+global.faall = fs.readFileSync('./media/image/fake.jpg')
 
 // Url
-global.mygit = 'https://github.com/Nexus07z'
+global.mygit = 'https://github.com/nazedev'
 global.myyt = 'https://youtube.com/c/Nazedev'
 global.myytv = 'https://youtu.be/FAsL-Jy4qLc'
 global.mygc = "https://chat.whatsapp.com/Cp1OJenk6Q9D9vgLjLU558"
 
 // Engak Ngaruh, Belum Kepasang
-global.botname = 'Nexusᴮᴼᵀ'
-global.apilol = '4fda13ee5ed767eef2174d23'
+global.botname = 'NAZE BOT'
 global.akulaku = 'Bot By Naze'
+global.ytname = 'YT NAZE' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
 
 
 let file = require.resolve(__filename)
--- index.js
+++ index.js
@@ -158,12 +158,24 @@ async function startNaze() {
                     ppgroup = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
                 }
                 
-                let butwel = [{ buttonId: `${prefix}menu`, buttonText: { displayText: 'MENU' }, type: 1 }]
-                let footermsj = ('')
-                let teks1 = `*Hola @${num.split('@')[0]}*\n\n*Te doy la bienvenida al grupo:*\n*${metadata.subject}*\n\n*Por favor lee las reglas de uso ingresando el siguiente comando:* \n\n*${prefix}reglas*\n\nPara ver todos los comandos de *${global.botname}* escribe el siguiente comando:\n\n*${prefix}menu*\n`
+                let butwel = [{ buttonId: 'menu', buttonText: { displayText: 'WELCOME' }, type: 1 }]
+                let butleav = [{ buttonId: 'subsyt', buttonText: { displayText: 'Sayonara👋' }, type: 1 }]
+                let butselamat = [{ buttonId: '', buttonText: { displayText: 'SELAMAT' }, type: 1 }]
+                let butsebar = [{ buttonId: '', buttonText: { displayText: 'SABAR' }, type: 1 }]
+                let nyoutube = ('© Naze\nYoutube/Sc :\nhttps://youtube.com/c/Nazedev')
+                let teks1 = `*Halo Kak @${num.split('@')[0]}*\n*Selamat Datang Di Grup*\n*${metadata.subject}*\n*Jangan Lupa Intro Yahh*\n_~Admin_`
+                let teks2 = `*Selamat Tinggal Kak @${num.split('@')[0]}*\n*Semoga Tenang Di Alam Sana*\n_~Admin_`
+                let teks3 = `*@${num.split('@')[0]} Promote From*\n*${metadata.subject}*\n*Selamat Anda Menjadi Admin*\n_~Jangan Semena Mena!_`
+                let teks4 = `*@${num.split('@')[0]} Demote From*\n*${metadata.subject}*\n_Kasihan Turun Pangkat🤭_`
                 if (anu.action == 'add') {
-                    naze.sendMessage(anu.id, { caption: teks1, location: { jpegThumbnail: await reSize(ppuser, 100, 100)}, buttons: butwel, footer: footermsj, mentions: [num] })
-                } 
+                    naze.sendMessage(anu.id, { caption: teks1, location: { jpegThumbnail: await reSize(ppuser, 100, 100)}, buttons: butwel, footer: nyoutube, mentions: [num] })
+                } else if (anu.action == 'remove') {
+                    naze.sendMessage(anu.id, { caption: teks2, location: { jpegThumbnail: await reSize(ppuser, 100, 100)}, buttons: butleav, footer: nyoutube, mentions: [num] })
+                } else if (anu.action == 'promote') {
+                    naze.sendMessage(anu.id, { caption: teks3, location: { jpegThumbnail: await reSize(ppuser, 100, 100)}, buttons: butselamat, footer: nyoutube, mentions: [num] })
+                } else if (anu.action == 'demote') {
+                    naze.sendMessage(anu.id, { caption: teks4, location: { jpegThumbnail: await reSize(ppuser, 100, 100)}, buttons: butsebar, footer: nyoutube, mentions: [num] })
+              }
             }
         } catch (err) {
             console.log(err)
